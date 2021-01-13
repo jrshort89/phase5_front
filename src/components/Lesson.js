fea1850666357f3587201538dd2740b98741c929
jrshort89 <jrshort89@gmail.com>
2021-01-13 15:36:42 -0500

    lessons menu

---
+++ src/components/Lesson.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Lesson() {
+  return <div>some text to be rendered for the lessons and stuff</div>;
+}
--- src/containers/Lessons.js
+++ src/containers/Lessons.js
@@ -18,6 +18,15 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import HomeIcon from "@material-ui/icons/Home";
 import { useSelector } from "react-redux";
+import Accordion from "../menus/Accordion";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+  Link,
+} from "react-router-dom";
+import Lesson from "../components/Lesson";
 
 const drawerWidth = 240;
 
@@ -73,7 +82,12 @@ function ResponsiveDrawer(props) {
           <ListItemIcon>
             <HomeIcon />
           </ListItemIcon>
-          <ListItemText primary={"Home"} />
+          <Link to="/lessons" className="sidemenu">
+            <ListItemText primary={"Home"} />
+          </Link>
+        </ListItem>
+        <ListItem>
+          <Accordion />
         </ListItem>
         {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
           <ListItem button key={text}>
@@ -157,21 +171,16 @@ function ResponsiveDrawer(props) {
       </nav>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-          dolor purus non enim praesent elementum facilisis leo vel. Risus at
-          ultrices mi tempus imperdiet. Semper risus in hendrerit gravida rutrum
-          quisque non tellus. Convallis convallis tellus id interdum velit
-          laoreet id donec ultrices. Odio morbi quis commodo odio aenean sed
-          adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-          integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-          eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-          quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-          vivamus at augue. At augue eget arcu dictum varius duis at consectetur
-          lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa sapien
-          faucibus et molestie ac.
-        </Typography>
+        <Switch>
+          <Route path="/lessons/lesson">
+            aasdfasdf
+            <Lesson />
+          </Route>
+          <Route path="/lessons/lesson2">
+            less 2
+            <Lesson />
+          </Route>
+        </Switch>
         <Typography paragraph>
           Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
           ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
